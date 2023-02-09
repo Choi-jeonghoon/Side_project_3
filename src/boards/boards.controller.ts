ddd0cce259577fceedfce471ef7a506a78f20fa9
J.hoon <68211978+Choi-jeonghoon@users.noreply.github.com>
2023-02-09 13:13:41 +0900

    Add:특정 ID값의 게시글 조회

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { Get, Param } from '@nestjs/common/decorators';
+import { Board } from 'src/board.entity';
 import { BoardsService } from './boards.service';
 
 @Controller('/boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Promise<Board> {
+    return this.boardsService.getBoardById(Number(id));
+  }
 }
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { Boards } from './boards';
 import { BoardsService } from './boards.service';
-// import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardRepository } from 'src/board.repositoty';
 import { TypeOrmExModule } from 'src/configs/typeorm-ex.module';
 
